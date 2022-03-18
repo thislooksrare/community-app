238c276265cc327a82748877cbff9b733138f916
LOOKS RARE <93809413+thislooksrare@users.noreply.github.com>
2022-03-18 07:22:40 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,14 @@ export default function Home() {
       <MainNavigation />
       <div className="max-w-2xl mx-auto pb-5 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <p className="text-lg mb-8">
-          To get started you can update your site name and your artist data.
+          Hello! We are LOOKS RARE.
         </p>
         <ol className="list-decimal list-inside">
-          <li>Site Name: src/config/settings.js</li>
-          <li>Artist Data: src/data/artists/artists.json</li>
+          <li>Find infos about the artists</li>
+          <li>see all auctions live</li>
         </ol>
         <p className="text-lg mt-8">
-          And the edit this page in src/pages/Home.jsx
+          we'll soon update with some statistics and tools, stay tuned!
         </p>
       </div>
     </div>
