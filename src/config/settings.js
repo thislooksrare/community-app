45b378ae93921ece243a9995e1b5d53d22669486
LOOKS RARE <93809413+thislooksrare@users.noreply.github.com>
2022-03-18 03:53:03 +0100

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -11,4 +11,4 @@ const config = process.env.REACT_APP_STAGE === "production" ? prod : dev;
 export const apiHost = config.apiHost;
 export const cdnUrl = "https://cache.21dao.xyz";
 export const rpcHost = "https://ssc-dao.genesysgo.net/";
-export const siteName = "Sample Community";
+export const siteName = "LOOKS RARE";
